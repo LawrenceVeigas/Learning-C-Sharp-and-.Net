d6bbcdec4b20b6e525eed8d64cc1c7411f36455f
LawrenceVeigas <lancev.veigas3@gmail.com>
2018-03-26 20:09:25 +0530

    new project

---
+++ chapter2/chapter2_numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace chapter2_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor),
+            args[0], true);
+            BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor),
+            args[1], true);
+            
+            try
+            {
+                WindowWidth = int.Parse(args[2]);
+                WindowHeight = int.Parse(args[3]);
+            }
+            catch(PlatformNotSupportedException)
+            {
+                WriteLine("PF not supported");
+            }
+            WriteLine($"There are {args.Length} arguments.");
+            foreach (var arg in args)
+            {
+            WriteLine(arg);
+            }
+            
+            
+        }
+    }
+}
---
+++ dup_char_in_string/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace dup_char_in_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hero = "KingOfTheJungleIsAlive";
+
+            string table = "";
+            string without_dup_chars = "";
+
+            foreach (char value in hero)
+            {
+                if(table.IndexOf(value)==-1)
+                {
+                    table += value;
+                    without_dup_chars += value;
+                }
+            }
+            Console.WriteLine(without_dup_chars);
+        }
+    }
+}
